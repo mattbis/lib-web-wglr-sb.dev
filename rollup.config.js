97caa4ef29922a556a499c15358dcfc07ebe5f9c
Matthew <863968+mattbis@users.noreply.github.com>
2023-08-02 19:53:12 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,2 +1,25 @@
 //const resolve = require('@rollup/plugin-node-resolve')
 //const terser = require('rollup-plugin-terser').terser
+/*module.exports = {
+    input: 'src/index.js',
+    exclude: ['src/package.json','src/package-lock.json'],
+    output: {
+        //file: '_dist/index.js',
+        format: 'cjs',
+        sourcemap: false,
+        // globals:{
+        //  jquery: '$'
+        // }
+    },
+    plugins: [
+        //nodeResolve(),
+        //commonjs(),
+        //json(),
+        terser({
+            output: {
+                comments: false
+            }
+        })
+    ]
+}
+*/
