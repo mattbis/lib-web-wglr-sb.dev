d1b4d67fbd5d94ba5f0cdf7910284731d4945086
Matthew <863968+mattbis@users.noreply.github.com>
2023-08-02 01:51:01 +0100

    Update fork-reference-dep-repos-locally-mattbis.sh

--- sh/local/fork-reference-dep-repos-locally-mattbis.sh
+++ sh/local/fork-reference-dep-repos-locally-mattbis.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/mattbis/ref-regl-insta-lines
 git clone https://github.com/mattbis/ref-regl-gpu-lines
 git clone https://github.com/mattbis/ref-regl-line
-git clone https://github.com/mattbis/regl
+git clone https://github.com/mattbis/ref-regl
 git clone https://github.com/mattbis/ref-webgl-lines
