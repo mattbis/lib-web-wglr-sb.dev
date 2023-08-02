ca7a10822f82a5820fa40d38145aabfd6fc10983
Matthew <863968+mattbis@users.noreply.github.com>
2023-08-02 20:07:39 +0100

    f: gotta stop must eat food .. didnt eat enough last night

--- src/lib/lib-web-wglr-sb.mjs
+++ src/lib/lib-web-wglr-sb.mjs
@@ -1,10 +1,13 @@
-export class WGLRSB {
+//export class WGLRSB {
+export default class G { /* WGLR_SB */
     static VERSION= '1.0.0'
+    static AUTHORS= []
 
     // allow with iterator ... * [Symbol.iterator](isInstance=this instanceof WGLR2)
 
     // runtime flags
     f= {
+        has_run: void 0
     }
 
     l= {} // gl layer
@@ -40,11 +43,15 @@ export class WGLRSB {
         rootElement= void 0,
         bgColour= void 0,
     }) {
-        const cvs= WGLR2['get_element']('canvas')
-        cvs['width']= w || Math['min'](WGLR2['win']['innerWidth'], WGLR2['win']['innerHeight'])
+        const cvs= G['get_element']('canvas')
+        cvs['width']= w || Math['min'](G['win']['innerWidth'], G['win']['innerHeight'])
         cvs['height']= h || Number(cvs['width'])
         this.width= Number(cvs['width'])
         this.height= Number(cvs['height'])
     }
+
+    /* STATICS */
+
+    static get_element() {}
     
 }
